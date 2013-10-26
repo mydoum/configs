3aca6d2b8e0ab95284e1a1bb47be1bc09658a3fa
mydoum <a.alonso.giraldo@gmail.com>
2013-10-26 18:46:54 +0800

    repos added

--- conf_server_centos.sh
+++ conf_server_centos.sh
@@ -14,3 +14,28 @@ sudo sed -i "" 's/Defaults    ! visiblepw/Defaults    visiblepw/g' /usr/sbin/vis
 
 sudo sed -i "" 's/#PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
 
+sudo /etc/init.d/sshd restart
+
+# Install App
+
+sudo yum install git
+sudo yum install zsh
+
+# Add repos
+
+sudo echo -e "[nginx]\
+\nname=nginx repo\
+\nbaseurl=http://nginx.org/packages/centos/$releasever/$basearch/\
+\ngpgcheck=0\
+\nenabled=1" > /etc/yum.repos.d/nginx.repo
+
+sudo echo -e "[mongodb]\
+\nname=MongoDB Repository\
+\nbaseurl=http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/\
+\ngpgcheck=0\
+\nenabled=1" > /etc/yum.repos.d/mongodb.repo
+
+# Install website services
+
+sudo yum install nginx
+sudo /etc/init.d/nginx start
