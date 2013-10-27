d5657e7889c617a8f7e16b992c78c2ea863d2774
mydoum <a.alonso.giraldo@gmail.com>
2013-10-27 21:51:32 +0800

    some others web applications

--- scratch.sh
+++ scratch.sh
@@ -63,12 +63,45 @@ if [ ! -d /Applications/Dash.app ]; then
 	rm -r __MACOSX
 fi
 
+#fixme  its for zipeg
+if [ ! -d /Applications/Zipeg.app ]; then
+	wget http://www.zipeg.net/downloads/zipeg_mac.dmg
+	hdiutil mount zipeg_mac.dmg
+	sudo cp -R /Volumes/Zipeg/Zipeg.app /Applications/
+	hdiutil unmount /Volumes/Zipeg/
+	rm zipeg_mac.dmg
+fi
+
+# Install specific Web aplications 
+
+if ! hash node 2> /dev/null; then
+  	echo "[install] Node.js"
+  	brew install node
+fi
+
+if ! hash npm 2> /dev/null; then
+  	echo "[install] Node Packet Manager"
+	brew install npm
+  	echo "export NODE_PATH=/usr/local/lib/node" >> ~/.bash_profile
+  	export PATH=/usr/local/share/npm/bin:$PATH
+	echo "dont forget to add this variable in your env"
+	echo "PATH=/usr/local/share/npm/bin:\$PATH"
+fi
+
 hash mongod &> /dev/null
 if [ $? -eq 1 ]; then
 	brew install mongodb
+	brew install bzr
 fi
 
 hash go &> /dev/null
 if [ $? -eq 1 ]; then
 	brew install go
+	mkdir $HOME/Documents/go
+	echo "export GOROOT=$HOME/go" >> ~/.bash_profile
+	echo "export GOPATH=$HOME/Documents/go" >> ~/.bash_profile
+	brew install python
+	echo "export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH" >> ~/.bash_profile
+	go get labix.org/v2/mgo
+	go get labix.org/v2/mgo/bson
 fi
